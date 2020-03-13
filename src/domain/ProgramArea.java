324f6dc59ef3b968819c810f0ef7b6439de53a43
Andreas Awouters <andreas.awouters@student.kuleuven.be>
2020-03-13 17:26:05 +0100

    programArea documentation

--- src/domain/ProgramArea.java
+++ src/domain/ProgramArea.java
@@ -12,16 +12,25 @@ public class ProgramArea {
 	private List<Block> topLevelBlocks;
 	private Block nextToExecute = null;
 	
-	
+	/**
+	 * Creates a new ProgramArea
+	 */
 	public ProgramArea() {
 		this.topLevelBlocks = new ArrayList<Block>();
 	}
 
-	
+	/**
+	 * 
+	 * @return number of top level blocks
+	 */
 	public int nbTopLevelBlocks() {
 		return topLevelBlocks.size();
 	}
 	
+	/**
+	 * 
+	 * @return  nbTopLevelBlocks() == 1 && top level block can be executed
+	 */
 	public boolean hasValidTopLevelBlock() {
 		if (nbTopLevelBlocks() == 1) {
 			Block topLevelBlock = topLevelBlocks.get(0);
@@ -38,22 +47,36 @@ public class ProgramArea {
 		}
 	}
 	
+	/**
+	 * 
+	 * @return nextToExecute != null
+	 */
 	public Boolean programInProgress() {
 		return nextToExecute != null;
 	}
 	
+	/**
+	 * 
+	 * @return true if program can start executing
+	 */
 	public Boolean canStartExecution() {
 		return (!programInProgress() && hasValidTopLevelBlock());
 		// TODO: maybe check if program is valid
 	}
 	
-	
+	/**
+	 * @post nextToExecute != null
+	 */
 	public void startExecution() {
 		if (canStartExecution()) {
 			nextToExecute = topLevelBlocks.get(0);
 		} 
 	}
 	
+	/**
+	 * 
+	 * @param gameController gameController to execute the next function in
+	 */
 	public void executeNextBlock(GameController gameController) {
 		try {
 			nextToExecute = nextToExecute.execute(gameController);
@@ -63,16 +86,29 @@ public class ProgramArea {
 		}
 	}
 	
+	/**
+	 * @post nextToExecute == null
+	 */
 	public void stopExecution() {
 		if (programInProgress()) {
 			nextToExecute = null;
 		}
 	}
 	
+	/**
+	 * 
+	 * @param block, block to add to topLevelBlocks
+	 */
 	public void addTopLevelBlock(Block block) {
 		topLevelBlocks.add(block);
 	}
 	
+	/**
+	 * 
+	 * @param block, block to remove from topLevelBlocks
+	 * @throws Exception
+	 * 			block is not a top level block
+	 */
 	public void removeTopLevelBlock(Block block) throws Exception {
 		if (topLevelBlocks.contains(block)) {
 			topLevelBlocks.remove(block);
@@ -81,19 +117,35 @@ public class ProgramArea {
 		}
 	}
 	
+	/**
+	 * 
+	 * @param block block to check
+	 * @return topLevelBlocks.contains(block)
+	 */
 	public boolean isTopLevelBlock(Block block) {
 		return topLevelBlocks.contains(block);
 	}
 	
-	
+	/**
+	 * 
+	 * @return nextToExecute
+	 */
 	public Block getNextBlockToExecute() {
 		return this.nextToExecute;
 	}
 	
+	/**
+	 * 
+	 * @return topLevelBlocks
+	 */
 	public List<Block> getTopBlocks() {
 		return this.topLevelBlocks;
 	}
 	
+	/**
+	 * 
+	 * @return copy of topLevelBlocks
+	 */
 	public List<Block> getAllBlocks() {
 		List<Block> list = new ArrayList<Block>();
 		for(Block block: this.getTopBlocks()) {
