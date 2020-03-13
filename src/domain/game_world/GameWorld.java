0419dc75b5db7b7dddd3aaed564beaaf323611ee
Geert Wesemael <onlinegeert@gmail.com>
2020-03-13 17:52:55 +0100

    Auto stash before merge of "feature/GUISplitup" and "origin/feature/GUISplitup"

--- src/domain/game_world/GameWorld.java
+++ src/domain/game_world/GameWorld.java
@@ -6,12 +6,23 @@ import domain.game_world.cell.*;
 
 // GameWorld contains a grid and the entities that move around on that grid
 // Currently only a single Robot moves around
+/**
+ * A class of gameWorlds, containing the grid, robot and startRobot.
+ * The startrobot is used to reset the game world.
+ *
+ */
 public class GameWorld {
 	
 	private Grid grid;
 	private Robot robot;
 	private Robot startRobot;
-	
+	/**
+	 * Initialize a new gameworld with a given grid and height. 
+	 * Also sets the startRobot and robot to the givenstartposition.
+	 * 
+	 * @param grid
+	 * @param startPosition
+	 */
 	public GameWorld(Grid grid, Vector startPosition) {
 		this.setGrid(grid);
 		this.startRobot = new Robot(startPosition);
@@ -19,30 +30,51 @@ public class GameWorld {
 	}
 
 	// Default 5x5 empty GameWorld with Robot at (0,0) and goal at (4, 4);
+	/**
+	 * Initialize an empty game world with the standard grid and the robot at (0,0).
+	 */
 	public GameWorld() {
 		this(new Grid(), new Vector(0, 0));
 	}
 	
 	// Create random GameWorld with given width and height;
+	/**
+	 * Initialize a gameworld with the given width and height.
+	 * @param width
+	 * @param height
+	 */
 	public GameWorld(int width, int height) {
 		this.setGrid(Grid.randomGrid(width, height));
 		Vector randomRobotLocation = this.getRandomRobotLocation();
 		this.startRobot = new Robot(randomRobotLocation, getRandomRobotDirection());
 		this.setRobot(new Robot(startRobot));
 	}
-
+	/**
+	 * @return grid of the gameworld.
+	 */
 	public Grid getGrid() {
 		return grid;
 	}
-
+	/**
+	 * Set grid of the current gameworld.
+	 * 
+	 * @param grid
+	 */
 	public void setGrid(Grid grid) {
 		this.grid = grid;
 	}
-	
+	/**
+	 * @return robot of the given gameworld
+	 */
 	public Robot getRobot() {
 		return robot;
 	}
 
+	/**
+	 * Set robot of the current gameworld.
+	 * 
+	 * @param robot
+	 */
 	public void setRobot(Robot robot) {
 		this.robot = robot;
 	}
@@ -51,6 +83,11 @@ public class GameWorld {
 	// Robot commands
 	
 	// True if the cell in front of the robot is a Wall
+	/**
+	 * Returns true if the cell in front is a wall.
+	 * 
+	 * @return true if cell in front of the robot is a wall.
+	 */
 	public boolean robotWallInFront() {
 		try {
 			Vector positionInFront = getRobot().getPositionInFront();
@@ -61,6 +98,11 @@ public class GameWorld {
 		
 	}
 	// True if the robots location is equal to the goal location
+	/**
+	 * Returns true if the robot is located on the same position as the goal.
+	 * 
+	 * @return true if the robot is on the same location as the goal.
+	 */
 	public boolean robotOnGoal() {
 		try {
 			Vector currentPosition =getRobot().getLocation();
@@ -70,7 +112,10 @@ public class GameWorld {
 			return false;
 		}
 	}
-	
+	/**
+	 * Set the location to the robot to the cell in front of the robot,
+	 * if this is allowed (no walls or end of grid).
+	 */
 	public void robotStepForwards() {
 		try {
 			Vector positionInFront = getRobot().getPositionInFront();
@@ -83,15 +128,23 @@ public class GameWorld {
 		}
 		
 	}
-	
+	/**
+	 * Set the direction of the robot to left.
+	 */
 	public void robotTurnLeft() {
 		getRobot().turnLeft();
 	}
-	
+	/**
+	 * Set the direction of robot to right.
+	 */
 	public void robotTurnRight() {
 		getRobot().turnRight();
 	}
-	
+	/**
+	 * Return a random location where the robot can stand.
+	 * 
+	 * @return a random location where the robot can stand.
+	 */
 	private Vector getRandomRobotLocation() {
 		Random rand = new Random();
 
--- src/domain/game_world/Robot.java
+++ src/domain/game_world/Robot.java
@@ -1,45 +1,92 @@
 package domain.game_world;
-
+/**
+ * A class of the Robot, containing the location and the direction of the robot.
+ *
+ */
 public class Robot {
 	
 	private Vector location;
 	private Direction direction;
-	
+	/**
+	 * Initialize a new robot on the given location and let him look in the given direction
+	 * 
+	 * @param location
+	 * @param direction
+	 */
 	public Robot(Vector location, Direction direction) {
 		this.setLocation(location);
 		this.setDirection(direction);
 	}
-	
+	/**
+	 * Initialize a new robot on the given location and let him look up.
+	 * 
+	 * @param location
+	 * @param direction
+	 */
 	public Robot(Vector location) {
 		this(location, Direction.UP);
 	}
-	
+	/**
+	 * Initialize a new robot on the same place as the given robot
+	 * and let him look in the same direction.
+	 * 
+	 * @param location
+	 * @param direction
+	 */
 	public Robot(Robot other) {
 		this(other.getLocation(), other.getDirection());
 	}
-	
+	/**
+	 * Initialize the robot with a given x and y coordinate looking up.
+	 * 
+	 * @param x
+	 * @param y
+	 */
 	public Robot(int x, int y) {
 		this(new Vector(x, y));
 	}
 	
-
+	/**
+	 * Return the location of the robot.
+	 * 
+	 * @return location of the robot.
+	 */
 	public Vector getLocation() {
 		return location;
 	}
-
+	/**
+	 * Set the location of the robot to the given location.
+	 * 
+	 * @param location
+	 */
 	public void setLocation(Vector location) {
 		this.location = location;
 	}
-
+	/**
+	 * Return the direction of the robot.
+	 * 
+	 * @return direction of the robot.
+	 */
 	public Direction getDirection() {
 		return direction;
 	}
-
+	/**
+	 * Set the direction of the robot to the given location.
+	 * 
+	 * @param direction
+	 */
 	public void setDirection(Direction direction) {
 		this.direction = direction;
 	}
 	
 	//given position may be out of bound!!
+	/**
+	 * Returns the location of cell in front of the robot.
+	 * 
+	 * @return location of the cell in front of the robot.
+	 * @throws Exception
+	 * 		   If the given location is not in bounds.
+	 */
 	public Vector getPositionInFront() throws Exception {
 		switch (this.direction) {
 			case UP:
@@ -56,6 +103,11 @@ public class Robot {
 	}
 	
 	// Actions
+	/**
+	 * Sets the location of the robot to the location in front of the robot,
+	 * if the location in front of the robot is a wall or not inside of the gird,
+	 * then the location is set to the current location.
+	 */
 	public void stepForwards() {
 		try {
 			Vector positionInFront = getPositionInFront();
@@ -65,7 +117,9 @@ public class Robot {
 		}
 		
 	}
-	
+	/**
+	 * Set the direction of the robot to the direction to the left of the current direction.
+	 */
 	public void turnLeft() {
 		switch (getDirection()) {
 			case DOWN:
@@ -88,7 +142,9 @@ public class Robot {
 				break;
 		}
 	}
-	
+	/**
+	 * Set the direction of the robot to the direction to the right of the current direction.
+	 */
 	public void turnRight() {
 		switch (getDirection()) {
 			case DOWN:
