1d76ef01a7582f9cc5cd5c217d4b87dd94a12c6d
Thomas <vanerumthomas@hotmail.com>
2020-04-24 12:30:46 +0200

    finished documentation command

--- Blockr/src/command/setConditionCommand.java
+++ Blockr/src/command/setConditionCommand.java
@@ -6,26 +6,31 @@ import domain.block.ConditionBlock;
 import domain.block.ImplementationBlock;
 import domain.block.SurroundingBlock;
 
-public class setConditionCommand implements Command{
+public class setConditionCommand implements Command {
 	ImplementationBlock BF = new ImplementationBlock();
-	
-	//first block of group of blocks that gets connected.
+
+	// first block of group of blocks that gets connected.
 	ConditionBlock blockToConnect;
-	//the surrounding block blockToConnect will be connected to.
+	// the surrounding block blockToConnect will be connected to.
 	SurroundingBlock surroundingBlock;
-	//block after group of blocks connected
+	// block after group of blocks connected
 	ConditionBlock nextBlock;
-	//The gamecontroller wher the blocks exist
+	// The gamecontroller wher the blocks exist
 	GameController GC;
 	ImplementationGameController GCF = new ImplementationGameController();
-	
+
 	/**
+	 * Makes a Command used to set the condition of a surrounding block.
 	 * 
 	 * @param blockToConnectTo block before group of blocks connected
-	 * @param blockToConnect first block of group of blocks that gets connected.
-	 * @param nextBlock block after group of blocks connected
+	 * @param blockToConnect   first block of group of blocks that gets connected.
+	 * @param nextBlock        block after group of blocks connected
+	 * 
+	 * @post the objects surroundingBlock, blockToConnect, nextBlock and GC are
+	 *       stored in this Command for later use.
 	 */
-	public setConditionCommand(SurroundingBlock surroundingBlock, ConditionBlock blockToConnect, ConditionBlock nextBlock, GameController GC) {
+	public setConditionCommand(SurroundingBlock surroundingBlock, ConditionBlock blockToConnect,
+			ConditionBlock nextBlock, GameController GC) {
 		this.blockToConnect = blockToConnect;
 		this.surroundingBlock = surroundingBlock;
 		this.nextBlock = nextBlock;
@@ -34,12 +39,12 @@ public class setConditionCommand implements Command{
 
 	@Override
 	public void execute() {
-		GCF.setCondition(surroundingBlock, blockToConnect, GC);	
+		GCF.setCondition(surroundingBlock, blockToConnect, GC);
 	}
 
 	@Override
 	public void undo() {
-		GCF.disconnect(blockToConnect,GC);
+		GCF.disconnect(blockToConnect, GC);
 		GCF.disconnect(nextBlock, GC);
 		GCF.setCondition(surroundingBlock, nextBlock, GC);
 	}
