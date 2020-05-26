aa0844e4d626c8e4a24a2ef412abebf746d2b654
Dirk Vanbeveren <vanbeverendirk@hotmail.com>
2020-05-26 13:42:58 +0200

    action/predicate docs

--- RobotGameWorld/src/actions/ActionExecution.java
+++ RobotGameWorld/src/actions/ActionExecution.java
@@ -4,6 +4,22 @@ import game_world.GameWorld;
 import game_world.api.Action;
 import game_world.api.ActionResult;
 
+/**
+ * Class responsible to execute actions
+ * 
+ * @version 4.0
+ * @author Andreas Awouters 
+ * 	       Thomas Van Erum 
+ * 		   Dirk Vanbeveren 
+ * 		   Geert Wesemael
+ *
+ */
 public abstract class ActionExecution implements Action{
+	/**
+	 * Execute this action on the given gameworld.
+	 * @param gameWorld
+	 *        The game world to execute the action on
+	 * @return An action result.
+	 */
 	public abstract ActionResult execute(GameWorld gameWorld);
 }
--- RobotGameWorld/src/actions/TurnRightAction.java
+++ RobotGameWorld/src/actions/TurnRightAction.java
@@ -2,7 +2,16 @@ package actions;
 
 import game_world.GameWorld;
 import game_world.api.ActionResult;
-
+/**
+ * Class that executes a turn right action.
+ * 
+ * @version 4.0
+ * @author Andreas Awouters 
+ * 	       Thomas Van Erum 
+ * 		   Dirk Vanbeveren 
+ * 		   Geert Wesemael
+ *
+ */
 public class TurnRightAction extends ActionExecution {
 
 	@Override
